6f615946a4886a46397f3c6b32c7af91c170c9da
ESENS12 <ghdldid12@gmail.com>
2019-03-20 19:11:52 +0900

    add flex(for test git push)

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -37,11 +37,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
+    flex: 1,
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
   instructions: {
+    flex: 1,
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
