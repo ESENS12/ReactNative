fc9c4d809f2939901fa41e30f4c32079fc753013
ESENS12 <ghdldid12@gmail.com>
2019-03-21 10:11:23 +0900

    flex test

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -1,51 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, Button, View} from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+  }
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+
+        <View style={styles.container}>
+          <View style={styles.topView }/>
+          <View style={styles.containerBox}/>
+          <View style={styles.bottomView}/>
+        </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 5,
+    backgroundColor: 'green' ,
   },
-  welcome: {
-    flex: 1,
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  topView: {
+    flex: 2.5,
+    backgroundColor: 'red' ,
   },
-  instructions: {
-    flex: 1,
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  containerBox: {
+    flex:1,
+    backgroundColor: 'white',
   },
+  bottomView:{
+    flex:2.5,
+    backgroundColor: 'blue' ,
+  }
+
 });
