743cada64a45846780af7dd4466282b153be6c26
ESENS12 <ghdldid12@gmail.com>
2019-03-22 13:10:35 +0900

    Add Component

--- gestureTest/App.js
+++ gestureTest/App.js
@@ -8,22 +8,14 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import TouchComponent from './component/TouchComponent';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TouchComponent/>
       </View>
     );
   }
