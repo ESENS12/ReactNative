5634f79234effb4b60c4f0582d65fba801394a4f
ESENS12 <ghdldid12@gmail.com>
2019-04-26 18:31:14 +0900

    Fixed problem

--- SnapCardViewTest/App.js
+++ SnapCardViewTest/App.js
@@ -1,49 +1,109 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
 
-import Carousel from 'react-native-snap-carousel';
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {SnapComponent} from "./SnapComponent";
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+import styled from "styled-components/native"; // 3.1.6
+import Carousel from 'react-native-snap-carousel'; // 3.6.0
 
 export default class App extends Component {
 
+  constructor(props){
+    super();
+    this.state = {
+      errors: []
+    }
+    this.props = props;
+    this._carousel = {};
+    this.init();
+  }
 
-  render() {
+  init(){
+    this.state = {
+      videos: [
+        {
+          id: "WpIAc9by5iU",
+          thumbnail: "https://img.youtube.com/vi/D9ioyEvdggk/hqdefault.jpg",
+          title: "Led Zeppelin - Stairway To Heaven"
+        }, {
+          id: "sNPnbI1arSE",
+          thumbnail: "https://img.youtube.com/vi/sNPnbI1arSE/hqdefault.jpg",
+          title: "Eminem - My Name Is"
+        }, {
+          id: "VOgFZfRVaww",
+          thumbnail: "https://img.youtube.com/vi/VOgFZfRVaww/hqdefault.jpg",
+          title: ""
+        }
+      ]
+    };
+
+    console.log("ThumbnailCarousel Props: ", this.props)
+  }
+
+  handleSnapToItem(index){
+    console.log("snapped to ", index)
+  }
+
+  _renderItem = ( {item, index} ) => {
+    console.log("rendering,", index, item)
     return (
-      <SnapComponent/>
+        <ThumbnailBackgroundView>
+          <CurrentVideoTO
+              onPress={ () => {
+                console.log("clicked to index", index)
+                this._carousel.snapToItem(index);
+              }}
+          >
+            <CurrentVideoImage source={{ uri: item.thumbnail }} />
+          </CurrentVideoTO>
+          {/*<NextVideoImage source={{ uri: this.state.currentVideo.nextVideoId }}/>*/}
+          <VideoTitleText>{item.title}</VideoTitleText>
+        </ThumbnailBackgroundView>
+    );
+  }
+
+  render = () => {
+
+    console.log("videos: updating")
+
+    return (
+        <CarouselBackgroundView>
+          <Carousel
+              ref={ (c) => { this._carousel = c; } }
+              data={this.state.videos}
+              renderItem={this._renderItem.bind(this)}
+              onSnapToItem={this.handleSnapToItem.bind(this)}
+              sliderWidth={360}
+              itemWidth={256}
+              layout={'default'}
+              firstItem={0}
+          />
+        </CarouselBackgroundView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+const VideoTitleText = styled.Text`
+  color: white;
+  top: 28;
+  justify-content: center;
+`
+const CurrentVideoImage = styled.Image`
+  top: 25;
+  box-shadow: 5px 10px;
+  width: 256;
+  height: 144;
+  border-radius: 10;
+`;
+
+const ThumbnailBackgroundView = styled.View`
+  justify-content: center;
+  align-items: center;
+  width: 256; 
+`;
+
+const CurrentVideoTO = styled.TouchableOpacity`
+`
+const CarouselBackgroundView = styled.View`
+  background-color: black;
+  height: 200;
+  width: 100%;
+`
