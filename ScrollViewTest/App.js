0b0c297927d0a929c648f8b7f532baedd58f4f92
ESENS12 <ghdldid12@gmail.com>
2019-04-29 12:55:25 +0900

    Add CustomAvatar Sample(with props, statement render, Left/Right Avatar)

--- ScrollViewTest/App.js
+++ ScrollViewTest/App.js
@@ -4,14 +4,18 @@ import {
 
 } from 'react-native';
 
-import {ListItem } from 'react-native-elements'
+import {Avatar, ListItem} from 'react-native-elements'
 import SectionListBasic from './component/SectionListBasic'
 import { randomUsers } from './util';
+import MyAvatar from "./component/MyAvatar";
 
 export default class App extends Component {
+
+
     state = {
         refreshing: false,
         data: randomUsers(20),
+        isRounded : false,
     };
 
     onEndReached = () => {
@@ -27,48 +31,94 @@ export default class App extends Component {
         this.setState({
             data: randomUsers(20),
         });
+        //alert("avatar uri : " + randomUsers(20)[0].avatar);
+
+
     };
 
     render() {
         return (
             {/*<SectionListBasic/>*/},
                 <View style={styles.container}>
+
+                    {/** 기본 아바타 사용 **/}
+
+                    {/*<FlatList*/}
+                        {/*data={this.state.data}*/}
+                        {/*initialNumToRender={20}*/}
+                        {/*onEndReachedThreshold={1}*/}
+                        {/*onEndReached={this.onEndReached}*/}
+                        {/*refreshing={this.state.refreshing}*/}
+                        {/*horizontal={true}*/}
+                        {/*onRefresh={this.onRefresh}*/}
+                        {/*renderItem={({ item }) => {*/}
+                            {/*return (*/}
+                                {/*<ListItem*/}
+                                    {/*roundAvatar*/}
+                                    {/*avatar={{uri: item.avatar}}*/}
+                                    {/*title={item.name}*/}
+                                {/*/>*/}
+                            {/*);*/}
+                        {/*}}*/}
+                    {/*/>*/}
+
+                    {/** 간단한 아바타이지만 커스텀이 어느정도 필요할 때 **/}
+
+                        {/*<FlatList*/}
+                            {/*data={this.state.data}*/}
+                            {/*initialNumToRender={20}*/}
+                            {/*onEndReachedThreshold={1}*/}
+                            {/*onEndReached={this.onEndReached}*/}
+                            {/*refreshing={this.state.refreshing}*/}
+                            {/*// horizontal={true}*/}
+                            {/*onRefresh={this.onRefresh}*/}
+                            {/*renderItem={({ item }) => {*/}
+                                {/*return (*/}
+                                    {/*<ListItem*/}
+                                        {/*roundAvatar*/}
+                                        {/*leftAvatar={<Avatar*/}
+                                        {/*source = {{ uri : item.avatar}}/>}*/}
+                                        {/*//// not avatar, using leftAvatar..*/}
+                                        {/*// leftAvatar={{*/}
+                                        {/*//     // size : "small","large", number*/}
+                                        {/*//     size : "medium",*/}
+                                        {/*//     source: {uri: item.avatar},*/}
+                                        {/*//     showEditButton : true,*/}
+                                        {/*//     // avatarStyle :styles.avatarStyle,*/}
+                                        {/*//*/}
+                                        {/*// }}*/}
+                                        {/*title={item.name}*/}
+                                        {/*containerStyle={styles.itemStyle}*/}
+                                    {/*/>*/}
+                                {/*);*/}
+                            {/*}}*/}
+                        {/*/>*/}
+
+
+                    {/** 커스텀 아바타 사용 방식 **/}
                     <FlatList
                         data={this.state.data}
                         initialNumToRender={20}
                         onEndReachedThreshold={1}
                         onEndReached={this.onEndReached}
                         refreshing={this.state.refreshing}
-                        horizontal={true}
+                        // horizontal={true}
                         onRefresh={this.onRefresh}
                         renderItem={({ item }) => {
                             return (
                                 <ListItem
-                                    roundAvatar
-                                    avatar={{uri: item.avatar}}
+                                    //roundAvatar
+                                    // 디폴트 props를 지정해뒀기 떄문에 만약 props에 값을 주는걸 까먹거나 null이나 none이 넘어가도 괜찮다.
+                                    leftAvatar={<MyAvatar/>}
+
+                                    //props에 따라 아바타 변경 적용
+                                    rightAvatar={<MyAvatar isRounded={false} uri={item.avatar} name={item.name}/>}
                                     title={item.name}
+                                    containerStyle={styles.itemStyle}
                                 />
                             );
                         }}
                     />
-                        <FlatList
-                            data={this.state.data}
-                            initialNumToRender={20}
-                            onEndReachedThreshold={1}
-                            onEndReached={this.onEndReached}
-                            refreshing={this.state.refreshing}
-                            // horizontal={true}
-                            onRefresh={this.onRefresh}
-                            renderItem={({ item }) => {
-                                return (
-                                    <ListItem
-                                        roundAvatar
-                                        avatar={{uri: item.avatar}}
-                                        title={item.name}
-                                    />
-                                );
-                            }}
-                        />
                 </View>
 
 
@@ -81,6 +131,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 22
+    },
+
+    avatarStyle:{
+        height:200,
+        width:200,
+    },
+    itemStyle:{
+        backgroundColor: 'rgba(247,247,247,1.0)',
     },
     sectionHeader: {
         paddingTop: 2,
--- ScrollViewTest/component/MyAvatar.js
+++ ScrollViewTest/component/MyAvatar.js
@@ -1,35 +1,70 @@
 import { Avatar } from 'react-native-elements';
 import React , {Component} from "react";
+import {StyleSheet} from "react-native";
 
 export default class MyAvatar extends Component {
 
+    static defaultProps = {
+        isRounded : true,
+        //보통 디폴트 uri 는 unknown image resources 나.. basic icon image 를 지정해서 정해둬야겠지
+        uri : 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        name : 'unknown'
+    };
+
     constructor(props) {
         super(props);
-        //this.state = {isRounded : true};
+        // this.state = { isRounded : true };
+        // this.isRounded = this.isRounded.bind(this);
     }
 
+    // isRounded = (value) =>
+        // this.setState({isRounded : value});
+
+
+    //props의 차이에 따라서 다르게 표현되도록 컴포넌트 조건부 리턴
     render() {
-        if(this.state.isRounded){
+        if(this.props.isRounded){
             return (
                 <Avatar
                     rounded
+                    title={this.props.name}
                     source={{
-                        uri:
-                            'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                        uri: this.props.uri,
                     }}
                 />
             );
         }else{
             return (
                 <Avatar
+                    title={this.props.name}
                     source={{
-                        uri:
-                            'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                        uri: this.props.uri,
                     }}
+                    avatarStyle={styles.avatarStyle}
+                    containerStyle={styles.containerStyle}
+                    //컨테이너가 아바타 보다 크게 잡힌 경우 남은 공간에 style을 줄 수 있다.(아바타에 여백을 주던가 컨테이너가 크던가..
+                    overlayContainerStyle={{backgroundColor:'blue'}}
                 />
             );
         }
     }
 }
+
+
+const styles = StyleSheet.create({
+
+    containerStyle:{
+        height: 70,
+        width:  70,
+    },
+
+    //avatarStyle은 컨테이너 내부 순수 이미지 스타일이라고 생각하면 됨
+    avatarStyle:{
+        flex :1,
+        margin : 3,
+    },
+
+});
+
 
 
