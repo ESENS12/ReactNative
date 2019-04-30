0690a6a0ab5f022caccca35d3651aa64020bf842
ESENS12 <ghdldid12@gmail.com>
2019-04-30 16:04:33 +0900

    Add global styles (js)

---
+++ PagerSample/component/styles.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+    StyleSheet,
+} = React;
+
+module.exports = StyleSheet.create({
+
+    viewPager: {
+        flex: 1,
+        justifyContent:'center',
+        alignContent:'stretch',
+        backgroundColor:'skyblue',
+    },
+
+});
