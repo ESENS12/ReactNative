4776b37dcc7fe5796ed347089811bd3905511862
ESENS12 <ghdldid12@gmail.com>
2019-10-02 09:20:47 +0900

    init Reboot Fockin-weather

---
+++ Reboot_fockin_weather/android/app/src/main/java/com/reboot_fockin_weather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.reboot_fockin_weather;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Reboot_fockin_weather";
+  }
+}
