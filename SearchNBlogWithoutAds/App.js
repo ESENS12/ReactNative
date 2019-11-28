52966b55dfdffcc9fc8e62e40289120dfe5b95b0
ESENS12 <ghdldid12@gmail.com>
2019-11-28 09:56:29 +0900

    Fix Some bug And Code clean-up

--- SearchNBlogWithoutAds/App.js
+++ SearchNBlogWithoutAds/App.js
@@ -23,9 +23,6 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-var itemIndex = 0;
-
-
 const fakeBlogKeywordList = ["스토리앤","seoulouba","revu","weble","ohmyblog","mrblog","tble","dinnerqueen"];
 
 /*
@@ -52,51 +49,38 @@ const fakeBlogKeywordList = ["스토리앤","seoulouba","revu","weble","ohmyblog
 //파블로체험단
 //디너의 여왕 -> https://dinnerqueen.net/
 
-async function getBlogResTest(searchQuery, page){
+async function getBlogRes(searchQuery, page){
 
-  //todo 검색어 edittext 텍스트 내용 바뀔때마다 setState해주면서 Re-rendering -> Crawling까지 .. UI 구조랑 백엔드 구조가 강결합되어있는 형태를 느슨하게 바꿔야함
-//todo  검색어 변경 입력 후 getNextPage하면 한 페이지 내에서 계속 돌리는 현상 발견됨
-  // var bodyObj = $('body')[0];  //tag로
-  // var divObj = $('#first')[0];  //id 값으로
-  // var inputJObjs = $('input');  // 해당 tag가 모두 접근된다.
-  // var titleObj = $('input[name=title]')[0]; // input tag이면서 name 속성값이 title인 DOM에 접근
-  // var buttonObj = $('input[type=button]')[0]; // tag와 type값으로 접근
-  // $(.'campaign_wrap).length => class로 판단
-  // $("#postListBody").length = id로 판단
 
-  // href="https://blog.naver.com/purekth2337?Redirect=Log&logNo=221577070707"
-  // https://blog.naver.com/PostView.nhn?blogId=purekth2337&logNo=221577070707
-  // blog.naver.com/PostView.nhn?blogId=dutxod2&logNo=221605239025&redirect=Dlog&widgetTypeCall=true&directAccess=true
-  // href="https://blog.naver.com/dabin8897/220595332941
+  /**
+   *  터치 -> 밑으로 expand되면서 imageView나옴 -> 어차피 Image는 광고여부판단시 수집하니까 image view에 그려준다음에 좌<->우 snap 방식으로 이미지만 넘겨보다가 아래 내용 클릭하면 해당 blog redirect
+   *
+   * */
 
-  // 블로그 게시물 체크할때  blogId, logNo(게시물번호) 이 2가지 정보 필요함
-
-  const Nblog_baseUrl = 'https://blog.naver.com/PostView.nhn?';
-  //Nblog_baseUrl + blogid=dutxod2&logNo=2215770970707;
 
   let ulList = [];
   const searchUrl = `https://search.naver.com/search.naver?query=${searchQuery}&sm=tab_pge&srchby=all&st=sim&where=post&start=${page}`;
   const response = await fetch(searchUrl);
   const htmlString = await response.text();
   const $ = cheerio.load(htmlString);
-
-  // const $bodyList = $("div.main_pack").children(".blog.section._blogBase._prs_blg").children("ul").children("li.sh_blog_top"); //클래스에 공백이 들어가는 경우는 .로 치환(모두 선택할수밖에 없음)
+  // console.log("search Url : "  + searchUrl);
   const $bodyList = $("li.sh_blog_top");
 
   console.log("result items : "+$bodyList.length);
-
   $bodyList.each(function(i, elem) {
-    itemIndex += 1;
+
     ulList[i] = {
       title: $(this).find('.sh_blog_title._sp_each_url._sp_each_title').attr('title'),
-      index : itemIndex,
+      // index : itemIndex,
       // title: $(this).find('strong[name=news-tl]').text(),
       url: $(this).find('.sh_blog_title._sp_each_url._sp_each_title').attr('href'),
       // image_url: $(this).find('p.poto a img').attr('src'),
       // date: $(this).find('span.p-time').text(),
       // lead : $(this).find('p.lead').text()
     };
+    // console.log("index : " + i + ", title : " + ulList[i].title);
   });
+  // console.log("===================================");
 
 
   //애초에 리스트에 add 할때 조건따라서 넣는 방식 or 검색결과 리스트와, 광고형, 비광고형 리스트 별도로 관리하던가(UI 레벨에서 광고형은 배경색이 있다던지 하는 형태로..?)
@@ -119,24 +103,25 @@ async function getBlogResTest(searchQuery, page){
     const htmlString = await response.text();
     const $ = cheerio.load(htmlString);
 
-    //todo blog.me type parsing exception
-
-    console.log('$() : ' + $("#viewTypeSelector").length);
+    // console.log('$() : ' + $("#viewTypeSelector").length);
 
     //$(".se-component.se-image a") 후손태그를 selector로 해서 , for 로 linkdata attr불러온다음 has contain 체크 후 ulList[i]에 boolean type 속성 추가 해주면 된다.
     console.log('$() image  : ' + $(".se-component.se-image a").length);
     // console.log('$() linkdata  : ' + $(".se-component.se-image a").attr('data-linkdata'));
     const $linkData = $(".se-component.se-image a");
+    let fakeNum = 0;
 
     $linkData.each(function(i, elem) {
       // console.log("i : " + i);
       // console.log("elem : " + elem);
       const atagData = $(this).attr('data-linkdata');
       // console.log(atagData);
+
       //todo negative or positive 방식 테스트 해봐야함
       if(atagData.includes("pstatic.net")){
-        console.log("maybe this is not Fake Tag");
+        fakeNum += 1;
       }
+
       if(atagData != null){
 
         for (let j = 0; j < fakeBlogKeywordList.length; j++) {
@@ -148,7 +133,9 @@ async function getBlogResTest(searchQuery, page){
         }
       }
     });
+    console.log("not Fake Image TagNum : " + fakeNum );
   }
+
 
   return ulList;
 }
@@ -156,7 +143,7 @@ async function getBlogResTest(searchQuery, page){
 const Item = props => (
 
     <TouchableOpacity style={styles.listItemParent}>
-      <Text style={styles.listItemText}> {props.index} </Text>
+      {/*<Text style={styles.listItemText}> {props.index} </Text>*/}
       <Text style={styles.listItemText}> {props.title} </Text>
       <Text style={styles.listItemURL}> {props.url} </Text>
       {/*<Text>{props.date}</Text>*/}
@@ -172,18 +159,10 @@ export default class App extends React.Component {
     this.state = {
       page : 1 ,
       items : [],
-      searchQuery : "맵당 신천점",
+      searchQuery : "강남 맛집",
       itemIndex : 1,
     };
   }
-
-  _onPressTest(){
-    const page = this.state.page+1;
-    console.log("onPress : " + page);
-    this.setState({page : page});
-  }
-
-  //todo 마지막 페이지 감지, 검색결과에서 광고, 비광고 list 거르기 , items에 push하니까 이전결과 날아감,(list array handling) 아니면 아예 방식 바꿔서 paging 처리..or infinity scroll style
 
   _searchIt(){
     const items = this.state.items;
@@ -191,21 +170,24 @@ export default class App extends React.Component {
     const itemIndex  = this.state.itemIndex;
 
     this.setState(state => {
-      return {items : [], page : 0, itemIndex : 0};
+      return {items : [], page : 1, itemIndex : 0};
     }, callback =>{
+      // console.log("searchit!" + "items : " + this.state.items + ", page : " + this.state.page + ", itemIndex : " + this.state.itemIndex);
       this.getNextPage();
     });
 
   }
 
+  //다음 페이지 로딩하여 Item Update(누적되지 않음)
   async getNextPage() {
 
-    const page = this.state.page+1;
+    const page = this.state.page+10;
     // const items = await getSportsNews(page);
-    const getItemRes  = await getBlogResTest(this.state.searchQuery,this.state.page);
+    const getItemRes  = await getBlogRes(this.state.searchQuery,this.state.page);
 
     this.setState( state => {
       return {items : this.state.items.concat(getItemRes), page};
+      // return {items : getItemRes, page};
     }, callback =>{
       // console.log("item size : " + this.state.items.length);
     });
@@ -216,6 +198,7 @@ export default class App extends React.Component {
     this.getNextPage();
   }
 
+  //리스트뷰지만 최대 10개까지만 표출한다
   render(){
     return(
         <View style={styles.container}>
@@ -235,7 +218,7 @@ export default class App extends React.Component {
             {this.state.items.map(item => <Item {...item} />)}
           </ScrollView>
           {/*<TouchableOpacity>*/}
-          <Button title="getNextPage" onPress={ ()=> this.getNextPage()}/>
+          <Button title="SearchMore" onPress={ ()=> this.getNextPage()}/>
           {/*</TouchableOpacity>*/}
           <Text style={styles.footer}>Crolling With Cheerio!</Text>
         </View>
