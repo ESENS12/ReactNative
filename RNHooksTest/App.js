b86587025ea62a7d952e3178ec2d43b987f1ee38
ESENS12 <ghdldid12@gmail.com>
2019-12-03 17:09:52 +0900

    Add CameraRoll hooks Sample

--- RNHooksTest/App.js
+++ RNHooksTest/App.js
@@ -1,13 +1,19 @@
 
-import React from 'react';
+import React , {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
+  Button,
   StatusBar,
+  PermissionsAndroid,
 } from 'react-native';
+
+import { useCameraRoll } from 'react-native-hooks'
+
+import { GetAlbum,GetPhoto } from './CameraRoll'
 
 import {
   Header,
@@ -17,15 +23,60 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+
 export default class App extends Component {
 
-  render() {
+  requestCameraPermission = async () =>{
+    try {
+      console.log("requestCameraPermission");
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        {
+          title: 'Cool Photo App Camera Permission',
+          message:
+              'Cool Photo App needs access to your camera ' +
+              'so you can take awesome pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('You can use the camera');
+        } else {
+          console.log('Camera permission denied');
+        }
+
+    } catch (err) {
+      console.warn("error! : "+err);
+    }
+  };
+
+
+  componentDidMount() {
+    if (Platform.OS === 'android') {
+      this.requestCameraPermission();
+      console.log('this is Android device');
+    } else {
+      console.log('this is iOS device');
+      // this.getLocation();
+    }
+  }
+
+
+  render(){
     return (
         <>
           <StatusBar barStyle="dark-content" />
+          <View style={styles.sectionContainer}>
+            {/*<Button title='Get Photos' onPress={() => getAlbum()}>Get Photos</Button>*/}
+            {/*<GetPhoto/>*/}
+            <GetAlbum/>
+          </View>
         </>
     );
   }
+  // }
 };
 
 const styles = StyleSheet.create({
@@ -35,6 +86,13 @@ const styles = StyleSheet.create({
   engine: {
     position: 'absolute',
     right: 0,
+  },
+  text:{
+    color: Colors.dark,
+    fontSize: 12,
+    marginTop : 100,
+    fontWeight: '600',
+    textAlign: 'center',
   },
   body: {
     backgroundColor: Colors.white,
---
+++ RNHooksTest/CameraRoll.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+import { useCameraRoll } from './myUseCameraRoll'
+
+import {
+    Button,
+    ScrollView,
+    Image,
+} from 'react-native';
+
+
+const initialState = {
+    edges: [],
+    page_info: {
+        end_cursor: '',
+        has_next_page: null,
+        start_cursor: ''
+    }
+};
+
+const defaultConfig = {
+    first: 20,
+    groupTypes: 'All'
+};
+
+let AlbumPhoto = [];
+
+export function GetAlbum() {
+    console.log("getAlbum : ");
+
+    const [photos, getPhotos, saveToCameraRoll] = useCameraRoll();
+
+    AlbumPhoto = photos.edges;
+
+    if(AlbumPhoto.length>0){
+        return (
+            <>
+                <ScrollView>
+                    {AlbumPhoto.map((p, i) => {
+                        return (
+                            <Image
+                                key={i}
+                                style={{
+                                    width: 300,
+                                    height: 100,
+                                }}
+                                source={{ uri: p.node.image.uri }}
+                            />
+                        );
+                    })}
+                </ScrollView>
+            </>
+        );
+    }else{
+        return(
+            <>
+                <Button title='Get Photos' onPress={() => getPhotos()}> Get Photos </Button>
+            </>
+        )
+
+    }
+}
