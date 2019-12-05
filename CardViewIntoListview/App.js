f52a1c01681b76a6408c7e2c4b46a3368c632fa3
ESENS12 <ghdldid12@gmail.com>
2019-12-05 09:18:35 +0900

    CarouselCardView in List Sample

--- CardViewIntoListview/App.js
+++ CardViewIntoListview/App.js
@@ -1,41 +1,13 @@
-import React, {useState, Component } from 'react';
-
-import styled from "styled-components/native"; // 3.1.6
-import Carousel from 'react-native-snap-carousel'; // 3.6.0
+import React, { Component } from 'react';
 
 import{
+    Picker,
     ScrollView,
     View,
 }from 'react-native';
 import {CarouselCardView} from './CarouselCardView';
 
-const Item = ( props ) => {
-    //
-    // if (props.isFake) {
-    //     return null
-    // }
-    console.log("props : " + props);
-    console.log("index : " + props.index);
-
-    return (
-        <CarouselBackgroundView>
-            <Carousel
-                ref={'carousel'}
-                data={props}
-                renderItem={this.refs['carousel']._renderItem(this)}
-                sliderWidth={360}
-                itemWidth={256}
-                layout={'default'}
-                firstItem={0}
-            />
-        </CarouselBackgroundView>
-    )
-};
-
-
-
 export default class App extends Component {
-
 
   constructor(props){
     super();
@@ -51,16 +23,19 @@ export default class App extends Component {
     this.state = {
       videos: [
         {
+            id : 'id1',
             thumbnails : [
               "https://img.youtube.com/vi/sNPnbI1arSE/hqdefault.jpg",
                "https://img.youtube.com/vi/sNPnbI1arSE/hqdefault.jpg",
                 "https://img.youtube.com/vi/sNPnbI1arSE/hqdefault.jpg",],
         }, {
+              id : 'id2',
               thumbnails :  [
                   "https://img.youtube.com/vi/D9ioyEvdggk/hqdefault.jpg",
                   "https://img.youtube.com/vi/D9ioyEvdggk/hqdefault.jpg",
                   "https://img.youtube.com/vi/D9ioyEvdggk/hqdefault.jpg",],
         }, {
+              id : 'id3',
               thumbnails :  [
                   "https://img.youtube.com/vi/VOgFZfRVaww/hqdefault.jpg",
                   "https://img.youtube.com/vi/VOgFZfRVaww/hqdefault.jpg",
@@ -71,28 +46,6 @@ export default class App extends Component {
     // console.log("ThumbnailCarousel Props: ", this.props)
   }
 
-  handleSnapToItem(index){
-    console.log("snapped to ", index)
-  }
-
-  _renderItem = ( {item, index} ) => {
-    console.log("rendering,", index, item);
-    return (
-        <ThumbnailBackgroundView>
-          <CurrentVideoTO
-              onPress={ () => {
-                console.log("clicked to index", index);
-                this._carousel.snapToItem(index);
-              }}
-          >
-            <CurrentVideoImage source={{ uri: item.thumbnails[0] }} />
-          </CurrentVideoTO>
-          {/*<NextVideoImage source={{ uri: this.state.currentVideo.nextVideoId }}/>*/}
-          {/*<VideoTitleText>{item.title}</VideoTitleText>*/}
-        </ThumbnailBackgroundView>
-    );
-  };
-
   render = () => {
 
     console.log("videos: updating");
@@ -102,69 +55,9 @@ export default class App extends Component {
 
         <View>
             <ScrollView>
-
-                <CarouselBackgroundView>
-                    <Carousel
-                        ref={ (c) => { this._carousel = c; } }
-                        data={this.state.videos}
-                        renderItem={this._renderItem.bind(this)}
-                        onSnapToItem={this.handleSnapToItem.bind(this)}
-                        sliderWidth={360}
-                        itemWidth={256}
-                        layout={'default'}
-                        firstItem={0}
-                    />
-                </CarouselBackgroundView>
-
-                <CarouselBackgroundView>
-                    <Carousel
-                        ref={ (c) => { this._carousel = c; } }
-                        data={this.state.videos}
-                        renderItem={this._renderItem.bind(this)}
-                        onSnapToItem={this.handleSnapToItem.bind(this)}
-                        sliderWidth={360}
-                        itemWidth={256}
-                        layout={'default'}
-                        firstItem={0}
-                    />
-                </CarouselBackgroundView>
-
-                {/*{ this.state.videos.map(item => <CarouselCardView {...item} />) }*/}
+                { this.state.videos.map(item => <CarouselCardView {...item} />) }
             </ScrollView>
-
-
         </View>
     );
   }
 }
-
-
-const VideoTitleText = styled.Text`
-  color: white;
-  top: 28;
-  justify-content: center;
-`;
-
-const CurrentVideoImage = styled.Image`
-  top: 25;
-  box-shadow: 5px 10px;
-  width: 256;
-  height: 144;
-  border-radius: 10;
-`;
-
-const ThumbnailBackgroundView = styled.View`
-  justify-content: center;
-  align-items: center;
-  width: 256; 
-`;
-
-const CurrentVideoTO = styled.TouchableOpacity`
-`;
-
-const CarouselBackgroundView = styled.View`
-  background-color: black;
-  height: 200;
-  width: 100%;
-`;
-
